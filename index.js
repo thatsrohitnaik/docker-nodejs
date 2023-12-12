d583ec7db989552a3178e1f369db82d79196e217
Rohit Naik Kundaikar <rohit24naik@gmail.com>
2023-12-12 19:24:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
    
-var server = app.listen(8000, function () {  
+var server = app.listen(3001, function () {  
   var host = server.address().address;  
   var port = server.address().port;  
   console.log('Example app listening at http://%s:%s', host, port);  
